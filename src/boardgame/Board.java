a10032e52ef9b147e0e2a03e7cbccff4988218e8
joseluizs <joseluiz.smarques@gmail.com>
2022-09-25 19:23:23 -0300

    Board
    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -28,4 +28,12 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) { //sobrecarga do método Piece
+		return pieces[position.getRow()][position.getColumn()];
+	}
+	
 }
